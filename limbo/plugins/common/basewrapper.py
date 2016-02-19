6e8706339bf2d10662d0145106be9f104cddddb3
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2016-02-19 19:59:22 +0000

    Basewrapper that keeps things in common between the different endpoints

---
+++ limbo/plugins/common/basewrapper.py
@@ -0,0 +1,55 @@
+import os
+
+class BaseWrapper(object):
+    def __init__(self):
+        if not os.environ.get('SD_AUTH_TOKEN'):
+            raise Exception('SD_AUTH_TOKEN is missing from environment')
+        self.token = os.environ.get('SD_AUTH_TOKEN')
+
+    def find_name(self, _id, services, devices):
+        for s in services:
+            if _id == s['_id']:
+                return s['name']
+        for d in devices:
+            if _id == d['_id']:
+                return d['name']
+        return 'No name'
+
+    def find_id(self, name, services, devices):
+        for s in services:
+            if name == s['name']:
+                return s['_id']
+        for d in devices:
+            if name == d['name']:
+                return d['_id']
+
+    def get_data(self, data, names=None):
+        """Inputs the data from the metrics endpoints and returns
+        the node that has contains the data + names of the metrics."""
+
+        if not names:
+            names = []
+        for d in data:
+            if d.get('data') or d.get('data') == []:
+                names.append(d.get('name'))
+                return d, names
+            else:
+                names.append(d.get('name'))
+                return self.get_data(d.get('tree'), names)
+
+    def metric_filter(self, metrics, filter=None):
+        """from a list of metrics ie ['cpuStats', 'CPUs', 'usr'] it constructs
+        a dictionary that can be sent to the metrics endpoint for consumption"""
+
+        metrics = list(metrics)
+        if not filter:
+            filter = {}
+            filter[metrics.pop()] = 'all'
+            return self.metric_filter(metrics, filter)
+        else:
+            try:
+                metric = metrics.pop()
+                dic = {metric: filter}
+                return self.metric_filter(metrics, dic)
+            except IndexError:
+                return metrics, filter
